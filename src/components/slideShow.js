1cb046251f6dcfb82383fe71331304b52345f7bb
jarrodyellets <jarrod524@gmail.com>
2018-05-10 19:50:49 +0200

    Formatted slideshow

--- src/components/slideShow.js
+++ src/components/slideShow.js
@@ -7,15 +7,15 @@ const SlideShow = (props) => {
 		  <Carousel.Item>
 		    <img className="slideImg slide1" alt="900x500" src="https://www.jarrodyellets.com/images/lumbar/slide2.jpg" />
 		    <Carousel.Caption>
-		      <h3>First slide label</h3>
-		      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+		      <h3>feel the good vertibrations</h3>
+		      <button className="slideButton">Shop all mattresses</button>
 		    </Carousel.Caption>
 		  </Carousel.Item>
 		  <Carousel.Item>
 		    <img className="slideImg slide2" alt="900x500" src="https://www.jarrodyellets.com/images/lumbar/slide1.jpg" />
 		    <Carousel.Caption>
-		      <h3>Second slide label</h3>
-		      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+		      <h3>Hal Kitzmiller signature mattress</h3>
+		      <button className="slideButton">Buy now</button>
 		    </Carousel.Caption>
 		  </Carousel.Item>
 		</Carousel>
