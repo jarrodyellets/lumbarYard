cbed4f641450383e425caa152a1b646e6b41b70d
jarrodyellets <jarrod524@gmail.com>
2018-05-16 10:30:07 +0200

    Finished footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,20 +4,29 @@ const Footer = (props) => {
 	return (
 		<div className="footerWrapper">
 			<div className="footerAddress">
-				<div>129 W 81st St</div>
-				<div>New York, NY 10024</div>
-			</div>
-			<div className="footerSocial">
-				<div>Follow The Lumbar Yard</div>
 				<div>
-					<i className="fab fa-facebook socialFoot"></i>
-					<i className="fab fa-instagram socialFoot"></i>
-					<i className="fab fa-twitter-square socialFoot"></i>
+					<div>129 W 81st St</div>
+					<div>New York, NY</div>
+				</div>
+				<div className="footerContact">
+					<div>jarrod@jarrodyellets.com</div>
+					<div>212-555-2390</div>
 				</div>
 			</div>
-			<div className="footerPayment">
-				<div><i class="fab fa-cc-visa card"></i><i class="fab fa-cc-mastercard card"></i><i class="fab fa-cc-discover card"></i></div>
-				<div><i class="fab fa-cc-amex card"></i><i class="fab fa-cc-apple-pay card"></i><i class="fab fa-cc-amazon-pay card"></i></div>
+			<div className="author">&copy; Jarrod Yellets | 2018</div>
+			<div className="footerRight">
+				<div className="footerSocial">
+						<div>Follow The Lumbar Yard</div>
+						<div>
+							<i className="fab fa-facebook socialFoot"></i>
+							<i className="fab fa-instagram socialFoot"></i>
+							<i className="fab fa-twitter-square socialFoot"></i>
+						</div>
+					</div>
+				<div className="footerPayment">
+					<div><i class="fab fa-cc-visa card"></i><i class="fab fa-cc-mastercard card"></i><i class="fab fa-cc-discover card"></i></div>
+					<div><i class="fab fa-cc-amex card"></i><i class="fab fa-cc-apple-pay card"></i><i class="fab fa-cc-amazon-pay card"></i></div>
+				</div>
 			</div>
 		</div>
 		)
