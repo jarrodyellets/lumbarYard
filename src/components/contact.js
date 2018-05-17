91e7cbd27e30bb4439914fc3421bef5581d5cc7c
jarrodyellets <jarrod524@gmail.com>
2018-05-17 12:15:52 +0200

    Finished Contact Page

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,28 +5,45 @@ const Contact = (props) => {
 		<div className="contactWrapper">
 			<div className="contactLeft">
 				<h1 className="contactTitle">Contact Us</h1>
-				<div className="contactText">We are here to answer any questions you may have about our products and services.
+				<div className="contactText">We are here to answer any questions you may have about our products or services.
 				Reach out to us and we'll respond as soon as we can.</div>
 				<div className="contactFormWrapper">
 					<form className="contactForm" method="POST" action="https://formspress.io/jarrod@jarrodyellets.com">
 						<div className="contactFormUpper">
 							<div className="contactLabel">
-								<label for="name">Name</label>
-								<input type="text" className="contactInput" name="name" id="name" required />
+								<label htmlFor="name">Name</label>
+								<input type="text" className="contactInput" name="name" id="name" size="35" required />
 							</div>
 							<div className="contactLabel">
-								<label for="email">Email</label>
-								<input type="email" className="contactInput" name="email" id="email" required />
+								<label htmlFor="email">Email</label>
+								<input type="email" name="email" id="email" size="35" required />
 							</div>
 						</div>
-						<label for="message">Message</label>
-						<textarea className="contactTextArea" name="text" id="text" required />
+						<label htmlFor="message">Message</label>
+						<textarea className="contactTextArea" name="text" id="text" rows="10" cols="60" required />
 						<button type="submit" className="contactButton">Submit</button>
 					</form>
 				</div>
 			</div>
 			<div className="contactRight">
-
+				<div className="contactContact">
+					<div className="contactBold">Email</div>
+					<div>jarrod@jarrodyellets.com</div>
+				</div>
+				<div className="contactContact">
+					<div className="contactBold">Telephone</div>
+					<div>212-555-2390</div>
+				</div>
+				<div className="contactContact">
+					<div className="contactBold">Address</div>
+					<div>129 W 81st St</div>
+					<div>New York, NY</div>
+				</div>
+				<div className="contactIcons">
+					<i className="fab fa-facebook socialTop"></i>
+					<i className="fab fa-instagram socialTop"></i>
+					<i className="fab fa-twitter-square socialTop"></i>
+				</div>
 			</div>
 		</div>
 		)
