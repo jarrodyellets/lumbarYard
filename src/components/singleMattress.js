f7b8e41b2eddec0ef056d390da64c6330fff3fc1
jarrodyellets <jarrod524@gmail.com>
2018-05-24 12:54:04 +0200

    Added size to cart function

--- src/components/singleMattress.js
+++ src/components/singleMattress.js
@@ -1,16 +1,11 @@
 import React from 'react';
 
 const SingleMattress = (props) => {
-  const size = props.size.map((size) => {
-    return(
-      <div key={props.price} className="singleSize">{size}</div>
-      )
-  })
   return (
     <div className="singleWrapper">
       <div className="singleTitle">{props.name}</div>
       <img src={props.image} className="singleImage" />
-      {size}
+      <div className="singleSize">Twin, Double, Queen, King</div>
       <div className="singlePrice">{props.price[0]} - {props.price[props.price.length -1]}</div>
       <button className="singleButton" onClick={props.handleCurrentMattress.bind(props.handleCurrentMatrress, props.mattress[props.index])}>Buy Now</button>
     </div>
--- src/data/mattress.js
+++ src/data/mattress.js
@@ -2,7 +2,7 @@ let mattress = [
 {
 	name: "Hal Kitzmiller Signature Mattress",
 	price: ["$899", "$1,099", "$1,599", "$1,799"],
-	size: ["Twin, Double, Queen, King"],
+	size: ["Twin", "Double", "Queen", "King"],
 	image: "https://www.jarrodyellets.com/images/lumbar/mat5.png",
 	description: "Designed by Hal himself, the Hal Kitzmiller Signature mattress will perfectly conform to you back shape.  This mattress is perfect for people who spend all day sitting on park benches.  Guarenteed not to smell like the East River.",
 	id: "kitz",
@@ -11,7 +11,7 @@ let mattress = [
 {
 	name: "Druidia Air Mattress",
 	price: ["$499", "$599", "$799", "$999"],
-	size: ["Twin, Double, Queen, King"],
+	size: ["Twin", "Double", "Queen", "King"],
 	image: "https://www.jarrodyellets.com/images/lumbar/mat1.png",
 	description: "Filled with air from Planet Drudidia, the Drudidia Air Mattress is the envy of the galaxy.  The mattress comes with a 1 year protection guarentee from Lone Star.",
 	id: "air",
@@ -20,7 +20,7 @@ let mattress = [
 {
 	name: "Kramerica Sand Mattress",
 	price: ["$799", "$999", "$1,299", "$1,599"],
-	size: ["Twin, Double, Queen, King"],
+	size: ["Twin", "Double", "Queen", "King"],
 	image: "https://www.jarrodyellets.com/images/lumbar/mat6.png",
 	description: "Ever dream of sleeping on the beach every night?  Now you can with the Kramerica Sand Mattress.  Filled with original Coney Island sand.",
 	id: "kram",
@@ -29,7 +29,7 @@ let mattress = [
 {
 	name: "NextGen Memory Foam Mattress",
 	price: ["$1,399", "$1,699", "$1,999", "$2,199"],
-	size: ["Twin, Double, Queen, King"],
+	size: ["Twin", "Double", "Queen", "King"],
 	image: "https://www.jarrodyellets.com/images/lumbar/mat3large.png",
 	description: "Developed by NASA, the NextGen Memory Foam Mattress will remember the contours of up to 6 people.  Supporting app is available via Android or iPhone.",
 	id: "next",
@@ -38,7 +38,7 @@ let mattress = [
 {
 	name: "Lumbar Yard Classic Mattress",
 	price: ["$399", "$499", "$699", "$999"],
-	size: ["Twin, Double, Queen, King"],
+	size: ["Twin", "Double", "Queen", "King"],
 	image: "https://www.jarrodyellets.com/images/lumbar/mat2.png",
 	description: "First mattress every designed by the Lumbar Yard.  Built with a mix of foam, latex and springs, this mattress will fit most sleeping needs.",
 	id: "lumb",
@@ -47,7 +47,7 @@ let mattress = [
 {
 	name: "Faux Feather Mattress",
 	price: ["$1,199", "$1,599", "$1,899", "$1,999"],
-	size: ["Twin, Double, Queen, King"],
+	size: ["Twin", "Double", "Queen", "King"],
 	image: "https://www.jarrodyellets.com/images/lumbar/mat4.png",
 	description: "Made with synthetic feathers, the vegan friendly Faux Feather Mattress is the perfect mattress for cold climates.",
 	id: "feat",
