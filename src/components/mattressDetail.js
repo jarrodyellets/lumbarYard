c43fe6d1ecc11d0d58dfe6726ddf40b639489cd8
jarrodyellets <jarrod524@gmail.com>
2018-05-25 10:10:52 +0200

    Mattress Detail Responsive

--- src/components/mattressDetail.js
+++ src/components/mattressDetail.js
@@ -6,6 +6,7 @@ const MattressDetail = (props) => {
       <div className="detailLeft">
         <div className="detailTitle">{props.currentMattress.name} Mattress</div>
         <div className="detailDetail">{props.currentMattress.description}</div>
+        <div className="detailPrice">{props.currentMattress.price[props.mattressIndex]}</div>
         <div className="detailSelectDiv">
           <div className="detailSelectText">Mattress Size</div>
           <select className="mattressSelect" onChange={props.handleIndex} value={props.mattressIndex}>
@@ -18,7 +19,6 @@ const MattressDetail = (props) => {
         <button className="detailButton" onClick={() => {props.handleCartAdd(props.currentMattress.name, props.currentMattress.price[props.mattressIndex], props.currentMattress.size[props.mattressIndex])}}>Add To Cart</button>
       </div>
       <div className="detailRight">
-        <div className="detailPrice">{props.currentMattress.price[props.mattressIndex]}</div>
         <img src={props.currentMattress.image} className="detailImage" />
       </div>
     </div>
