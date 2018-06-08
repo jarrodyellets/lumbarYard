ee59585645961e726b80b6644df14dde91ef9b61
jarrodyellets <jarrod524@gmail.com>
2018-06-08 09:55:16 +0200

    Added payment form

--- src/components/address.js
+++ src/components/address.js
@@ -6,31 +6,36 @@ const Address = (props) => {
       <h3>Delivery Address</h3>
       <form className="addressForm" onSubmit={() => {props.handleSection("payment")}}>
         <label htmlFor="name">Name</label>
-        <input className="checkoutName checkoutInput" type="text" 
+        <input className="checkoutInput" type="text" 
                                                       name="name" 
                                                       value={props.customer.name}
+                                                      autoComplete="name"
                                                       onChange={(e) => {props.handleCustomer(e, "name")}} />
         <label htmlFor="street">Street</label>
-        <input className="checkoutName checkoutInput" type="text" 
+        <input className="checkoutInput" type="text" 
                                                       name="street" 
                                                       value={props.customer.street}
+                                                      autoComplete="address-level1"
                                                       onChange={(e) => {props.handleCustomer(e, "street")}} />
         <label htmlFor="city">City</label>
-        <input className="checkoutName checkoutInput" type="text" 
+        <input className="checkoutInput" type="text" 
                                                       name="city" 
                                                       value={props.customer.city}
+                                                      autoComplete="address-level2"
                                                       onChange={(e) => {props.handleCustomer(e, "city")}} />
         <label htmlFor="state">State</label>
-        <input className="checkoutName checkoutInput" type="text" 
+        <input className="checkoutInput" type="text" 
                                                       name="state" 
                                                       value={props.customer.state}
+                                                      autoComplete="address-level2"
                                                       onChange={(e) => {props.handleCustomer(e, "state")}} />
         <label htmlFor="zip">Zip Code</label>
-        <input className="checkoutName checkoutInput" type="text" 
+        <input className="checkoutInput" type="text" 
                                                       name="zip" 
                                                       value={props.customer.zip}
+                                                      autoComplete="postal-code"
                                                       onChange={(e) => {props.handleCustomer(e, "zip")}} />
-        <button className="checkoutButton" type="submit" value="submit">Next</button>
+        <button className="checkoutButton" type="submit">Next</button>
       </form>
     </div>
     )
--- src/components/payment.js
+++ src/components/payment.js
@@ -4,24 +4,42 @@ const Payment = (props) => {
   return(
     <div className="paymentWrapper">
       <h3>Payment</h3>
-      <label htmlFor="cardType">Card Type</label>
-      <select className="checkoutInput">
-        <option>Visa</option>
-        <option>Mastercard</option>
-        <option>Discover</option>
-        <option>American Express</option>
-        <option>Apple Pay</option>
-        <option>Amazon Pay</option>
-      </select>
-      <label htmlFor="cardNumber">Card Number</label>
-      <input className="checkoutName checkoutInput" type="text" name="cardNumber" />
-      <label htmlFor="cardName">Card Name</label>
-      <input className="checkoutName checkoutInput" type="text" name="cardName" />
-      <label htmlFor="experation">Experation Date</label>
-      <input className="checkoutName checkoutInput" type="text" name="experation" />
-      <label htmlFor="code">Security Code</label>
-      <input className="checkoutName checkoutInput" type="text" name="code" />
-      <button className="checkoutButton" type="submit" value="submit">Next</button>
+      <form className="paymentForm" onSubmit={() => {props.handleSection("review")}}>
+        <label htmlFor="cardType" >Card Type</label>
+        <select className="checkoutInput" value={props.customer.cardType} autoComplete="cc-type" onChange={(e) => {props.handleCustomer(e, "cardType")}} >
+          <option value="visa" >Visa</option>
+          <option value="mastercard" >Mastercard</option>
+          <option value="discover ">Discover</option>
+          <option value="amercianExpress" >American Express</option>
+          <option value="applePay" >Apple Pay</option>
+          <option value="amazonPay" >Amazon Pay</option>
+        </select>
+        <label htmlFor="cardNumber">Card Number</label>
+        <input className="checkoutInput" type="text" 
+                                                      name="cardNumber"
+                                                      value={props.customer.cardNumber}
+                                                      autoComplete="cc-number"
+                                                      onChange={(e) => {props.handleCustomer(e, "cardNumber")}} />
+        <label htmlFor="cardName">Card Name</label>
+        <input className="checkoutInput" type="text" 
+                                                      name="cardName" 
+                                                      value={props.customer.cardName} 
+                                                      autoComplete="cc-name"
+                                                      onChange={(e) => {props.handleCustomer(e, "cardName")}} />
+        <label htmlFor="experation">Experation Date</label>
+        <input className="checkoutInput" type="text" 
+                                                      name="experation" 
+                                                      value={props.customer.exDate}
+                                                      autoComplete="cc-exp"
+                                                      onChange={(e) => {props.handleCustomer(e, "exDate")}} />
+        <label htmlFor="code">Security Code</label>
+        <input className="checkoutInput" type="text" 
+                                                      name="code" 
+                                                      value={props.customer.code}
+                                                      autoComplete="cc-csc"
+                                                      onChange={(e) => {props.handleCustomer(e, "code")}} />
+        <button className="checkoutButton" type="submit">Next</button>
+      </form>
     </div>
   )
 }
