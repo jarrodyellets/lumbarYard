15c66784883efdfc976008651bbf7bcf476f9825
jarrodyellets <jarrod524@gmail.com>
2018-07-02 01:00:22 +0200

    Added semicolon contact form

--- src/components/contact.js
+++ src/components/contact.js
@@ -11,15 +11,15 @@ const Contact = () => {
 					<form className="contactForm" method="POST" action="https://formspress.io/jarrod@jarrodyellets.com">
 						<div className="contactFormUpper">
 							<div className="contactLabel">
-								<label htmlFor="name">Name</label>
+								<label htmlFor="name">Name:</label>
 								<input type="text" className="contactInput input" name="name" id="name" size="35" required />
 							</div>
 							<div className="contactLabel">
-								<label htmlFor="email">Email</label>
+								<label htmlFor="email">Email:</label>
 								<input className="input" type="email" name="email" id="email" size="35" required />
 							</div>
 						</div>
-						<label htmlFor="message">Message</label>
+						<label htmlFor="message">Message:</label>
 						<textarea className="contactTextArea input" name="text" id="text" rows="10" cols="60" required />
 						<button type="submit" className="contactButton">Submit</button>
 					</form>
