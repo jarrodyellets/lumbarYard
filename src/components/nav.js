cbeabf3df8bc6b6247593e8b35d7a85cde81fba3
jarrodyellets <jarrod524@gmail.com>
2018-07-04 21:29:05 +0200

    Added hover class

--- src/components/nav.js
+++ src/components/nav.js
@@ -7,9 +7,9 @@ const Nav = (props) => {
 			<div className="linksWrapper">
 				<div className="linksDiv" id="linksDiv">
 					<a href="javascript:void(0)" className="closeBtn" onClick={props.handleClose}><i className="fas fa-times"></i></a>
-					<a href="#products" className="links" onClick={()=> {props.handlePage("mattresses")}}>Mattresses</a>
-					<a href="#about" className="links aboutLink" onClick={()=> {props.handlePage("about")}}>About</a>
-					<a href="#contact" className="links contactLink" onClick={()=> {props.handlePage("contact")}}>Contact</a>
+					<a href="#products" className="links hover" onClick={()=> {props.handlePage("mattresses")}}>Mattresses</a>
+					<a href="#about" className="links aboutLink hover" onClick={()=> {props.handlePage("about")}}>About</a>
+					<a href="#contact" className="links contactLink hover" onClick={()=> {props.handlePage("contact")}}>Contact</a>
 				</div>
 				<a href="#cart" className="cart" onClick={()=> {props.handlePage("cart")}}>
 					<i className="fas fa-shopping-cart"></i>
