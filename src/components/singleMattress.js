6c136b47fda07b911f40d09f5d0bf91fc397d8e3
jarrodyellets <jarrod524@gmail.com>
2018-09-03 12:08:15 +0200

    Added propTypes

--- src/components/singleMattress.js
+++ src/components/singleMattress.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const SingleMattress = (props) => {
   return (
@@ -7,9 +8,18 @@ const SingleMattress = (props) => {
       <img src={props.image} className="singleImage" alt="current mattress" />
       <div className="singleSize">Twin, Double, Queen, King</div>
       <div className="singlePrice">{props.price[0]} - {props.price[props.price.length -1]}</div>
-      <button className="singleButton" onClick={props.handleCurrentMattress.bind(props.handleCurrentMatrress, props.mattress[props.index])}>Buy Now</button>
+      <button className="singleButton" onClick={props.handleCurrentMattress.bind(props.handleCurrentMattress, props.mattress[props.index])}>Buy Now</button>
     </div>
     )
+}
+
+SingleMattress.propTypes = {
+  name: PropTypes.string,
+  image: PropTypes.string,
+  price: PropTypes.string,
+  handleCurrentMattress: PropTypes.func,
+  index: PropTypes.number,
+  mattress: PropTypes.object
 }
 
 export default SingleMattress;
