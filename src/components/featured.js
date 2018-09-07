7f023b2b1995607f5d4ed10c9eba93fd40f720c6
jarrodyellets <jarrod524@gmail.com>
2018-09-07 10:33:34 +0200

    Added props validation

--- src/components/featured.js
+++ src/components/featured.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Featured = (props) => {
   return(
@@ -14,14 +15,19 @@ const Featured = (props) => {
       <div className="featuredRight">
         <div className="featuredRightDescription">
           <div className="featuredRightReviewTitle">Best air mattress in the galaxy</div>
-          <div>"After fighting Planet Spaceball all day, coming home to the Druidia Air Mattress has been a lifesaver.  
-          Every morning I awake refreshed and ready to do battle with Dark Helmet."</div>
+          <div>&quot;After fighting Planet Spaceball all day, coming home to the Druidia Air Mattress has been a lifesaver.  
+          Every morning I awake refreshed and ready to do battle with Dark Helmet.&quot;</div>
           <div>-Lone Star-</div>
         </div>
         <img className="featuredRightImg" src="https://www.jarrodyellets.com/images/lumbar/mat1war.png" alt="NexGen Memory foam mattress" />
       </div>
     </div>
-    )
+  )
+}
+
+Featured.propTypes = {
+  handleCurrentMattress: PropTypes.func,
+  mattress: PropTypes.array
 }
 
 export default Featured;
