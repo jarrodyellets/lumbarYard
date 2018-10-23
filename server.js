a8bd0f5d7b7a194488d899e72738acbb3a0c57e2
jarrodyellets <jarrod524@gmail.com>
2018-10-23 10:42:04 +0200

    Removed express

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
-var path = require('path');
-var express = require('express');
+'use strict';
 
-var app = express();
+const Hapi = require('hapi');
+const Path = require('path');
 
-app.use(express.static(path.join(__dirname, 'dist')));
-app.set('port', process.env.PORT || 8080);
+const server = new Hapi.Server({
+  port: process.env.PORT || 8000,
+})
 
-var server = app.listen(app.get('port'), function() {
-  // eslint-disable-next-line no-console
-  console.log('listening on port ', server.address().port);
-});
+const init = async () => {
+
+  await server.register(require('inert'));
+
+  server.route({
+    method: 'GET',
+    path: '/{path*}',
+    handler: {
+      directory: {
+        path: Path.join(__dirname, 'dist'),
+        listing: false,
+        index: true
+      }
+    }
+  })
+
+  try {
+    await server.start();
+    console.log('Server started');
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+}
+
+init();
