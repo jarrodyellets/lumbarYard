74c58b4597c9315973ee1efeb53b73589c9ef1d7
jarrodyellets <jarrod524@gmail.com>
2018-10-23 10:04:22 +0200

    Added hapi server

---
+++ serverH.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const Hapi = require('hapi');
+const Path = require('path');
+
+const server = new Hapi.Server({
+  port: process.env.PORT || 8000,
+  routes: {
+    files: {
+      relativeTo: Path.join(__dirname, 'dist')
+    }
+  }
+})
+
+const init = async () => {
+  try {
+    await server.start();
+    console.log('Server started');
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+}
